cb62e2cd753ffd5a3523d4e1aa860b333198bbbb
M4Tv3Y <m.tymchenko@protonmail.ch>
2022-09-06 11:14:02 +0200

    removed urlcreateiso

--- win10debloat.ps1
+++ win10debloat.ps1
@@ -491,13 +491,6 @@ $Label11.height                  = 10
 $Label11.location                = New-Object System.Drawing.Point(687,659)
 $Label11.Font                    = New-Object System.Drawing.Font('Microsoft Sans Serif',12)
 
-$urlcreateiso                    = New-Object system.Windows.Forms.Button
-$urlcreateiso.text               = "Create Custom ISO"
-$urlcreateiso.width              = 232
-$urlcreateiso.height             = 30
-$urlcreateiso.location           = New-Object System.Drawing.Point(646,790)
-$urlcreateiso.Font               = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
-
 $ncpa                            = New-Object system.Windows.Forms.Button
 $ncpa.text                       = "Network Connections"
 $ncpa.width                      = 211
@@ -583,7 +576,7 @@ $restorepower.height             = 30
 $restorepower.location           = New-Object System.Drawing.Point(4,159)
 $restorepower.Font               = New-Object System.Drawing.Font('Microsoft Sans Serif',12)
 
-$Form.controls.AddRange(@($Panel1,$Panel2,$Label3,$Label15,$Panel4,$PictureBox1,$Label1,$Panel3,$ResultText,$Label10,$Label11,$urlcreateiso))
+$Form.controls.AddRange(@($Panel1,$Panel2,$Label3,$Label15,$Panel4,$PictureBox1,$Label1,$Panel3,$ResultText,$Label10,$Label11))
 $Panel1.controls.AddRange(@($brave,$firefox,$7zip,$adobereade,$gchrome,$vlc,$powertoys,$winterminal,$vscode,$Label2,$everythingsearch,$gimp,$Label7,$Label8,$Label9,$advancedipscanner,$putty,$etcher,$translucenttb,$githubdesktop,$discord))
 $Panel2.controls.AddRange(@($backgroundapps,$cortana,$actioncenter,$darkmode,$performancefx,$onedrive,$lightmode,$essentialundo,$EActionCenter,$ECortana,$RBackgroundApps,$HTrayIcons,$EClipboardHistory,$ELocation,$InstallOneDrive,$removebloat,$reinstallbloat,$WarningLabel,$Label5,$appearancefx,$STrayIcons,$EHibernation,$dualboottime))
 $Panel4.controls.AddRange(@($defaultwindowsupdate,$securitywindowsupdate,$Label16,$Label17,$Label18,$Label19,$windowsupdatefix,$disableupdates,$enableupdates,$Label12))
@@ -643,10 +636,6 @@ $vscode.Add_Click({
     winget install -e Microsoft.VisualStudioCode --source winget | Out-Host
     if($?) { Write-Host "Installed Visual Studio Code" }
     $ResultText.text = "`r`n" + "Finished Installing Visual Studio Code" + "`r`n" + "`r`n" + "Ready for Next Task"
-})
-
-$urlcreateiso.Add_Click({
-    Start-Process "https://youtu.be/R6XPff38iSc"
 })
 
 $winterminal.Add_Click({
